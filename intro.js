a20b9aa98018f7b6de20d066e7e17f5ac7a2b359
Israel <israelobiogumu@gmail.com>
2026-09-09 21:26:40 +0100

    My First Project with BeTechified

---
+++ intro.js
@@ -0,0 +1,4 @@
+const full_name = "Obiogumu Israel"
+const age = 25
+const fav_program = "Python"
+console.log("My name is " + full_name + "and I am " + age + " years old. My favourite programming language is " + fav_program)
